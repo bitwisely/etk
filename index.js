be6320564db90c25f091794974c20d703b53e9b7
Saltuk Alakus <saltukalakus@gmail.com>
2015-09-18 11:28:09 +0300

    jsdoc changes cont.

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
+
 Esq = require('esq');
 Module.exports = Etk;
 
 function Etk(client, opt) {
     this.client = client;
+    // Add tk namespace to elastic search object.
     this.client.tk = this.client.tk || {
-        // Quick search of items
-        searchQ: function (param, cb) {
-            this.client.search({
-                index: this.index,
-                type: this.type,
-                q: param},
-                cb);
-        },
 
+        /**
+         * Searches the key-value pair. Return result to the callback
+         * function with a signature of (err obj, response obj)
+         *
+         * @example
+         *     elastic = require('elasticsearch');
+         *     client = Etk(elastic, {index: "myindex", type: "mytype"});
+         *     client.tk.search("foo", "bar", function (err, resp) {
+         *        ...
+         *        });
+         * @param key Key to search
+         * @param value Value of the key
+         * @param cb Callback function
+         */
         search: function (key, value, cb) {
             var search_text = key + ":" + value;
-            client.search({
+            this.client.search({
                 index: this.index,
                 type: this.type,
                 body: search_text},
                 cb);
         },
 
+        /**
+         * Searches the key-value pair for the last number of days.
+         * Return result to the callback function with with a signature
+         * of (err obj, response obj)
+         *
+         * @param key Key to search
+         * @param value Value of the key
+         * @param days Number of days back to search
+         * @param cb Callback function
+         */
         searchLastDays: function (key, value, days, cb) {
             var esq = new Esq();
             var search_days = "now-" + days.toString() + "d/d";
@@ -31,7 +49,9 @@ function Etk(client, opt) {
             this.client.search({
                 body: query},
                 cb);
-        },
+        }
+        /*
+        ,
 
         searchBetween: function (key, value, start, end, cb) {
             var json_data = {"query": {
@@ -54,11 +74,16 @@ function Etk(client, opt) {
                     body: json_data},
                 cb);
         }
+        */
     };
 
+    // Store elastic search client for etk use
     this.client.tk.client = this.client;
-    this.client.tk.time_field = opt.time_field || "@timestamp"; // Default time field is Logstash compatible
+    // Default time field is Logstash compatible
+    this.client.tk.time_field = opt.time_field || "@timestamp";
+    // Default searches "all" available index
     this.client.tk.index = opt.index || "*";
+    // Default searches "all" available types
     this.client.tk.type = opt.type || "*";
     return this.client;
 }
