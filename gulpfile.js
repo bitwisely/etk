0d153e796986266a76ed73baa83dde0ffaaf5150
Saltuk Alakus <saltukalakus@gmail.com>
2015-09-18 15:44:29 +0300

    document generate cont.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var browserify = require('gulp-browserify');
 var shell = require('gulp-shell');
+var replace = require('gulp-replace');
 
 gulp.task('jshint', function () {
     gulp.src('./*.js')
@@ -20,13 +21,37 @@ gulp.task('jsbuild', function() {
 });
 
 // Document the API
-gulp.task('document', shell.task([
-    'cp ./confs/jsdoc-conf.json ./node_modules/jsdoc/conf.json',
+gulp.task('api_document', shell.task([
+    'cp ./confs/jsdoc/jsdoc-conf.json ./node_modules/jsdoc/conf.json',
     './node_modules/jsdoc/jsdoc.js index.js \
      -t ./node_modules/ink-docstrap/template \
-     -c ./node_modules/jsdoc/conf.json',
+     -c ./node_modules/jsdoc/conf.json'
+]));
+
+gulp.task('annotate_document', shell.task([
     './node_modules/docco/bin/docco index.js'
 ]));
+
+// Document api web site patches
+gulp.task('patch_api_document', ['api_document'], function(){
+    gulp.src(['out/global.html'])
+        .pipe(replace("</body>\n</html>",
+            "<style> .type-signature { font-size:50px;} .page-title {display: none;}</style>" +
+            "<script type='text/javascript' src='scripts/jquery-watch-element.js'></script>" +
+            "<script>$( document ).ready(function(){$('.toc-h1').waitUntilExists(" +
+                "function(){$('.toc-h1').hide(); " +
+                "$('.toc-h2').hide()}, " +
+                "false, true);});" +
+            "</script>" +
+            "</body>\n</html>"))
+        .pipe(gulp.dest('out'));
+});
+
+gulp.task('patch_files_copy', ['api_document'], shell.task([
+    'cp ./confs/jsdoc/jquery-watch-element.js ./out/scripts'
+]));
+
+gulp.task('document', ['annotate_document', 'patch_api_document', 'patch_files_copy']);
 
 gulp.task('watch', function () {
     gulp.watch('./*.js', ['jshint']);
